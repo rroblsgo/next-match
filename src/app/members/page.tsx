50744b906958987a8c4e0a058e0ea6b53232a34c
Ricardo Robles <rroblesgo@gmail.com>
2024-05-13 12:32:40 +0200

    End of Chapter 2 - Introduction and Basics

---
+++ src/app/members/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function MembersPage() {
+  return (
+    <div>
+      <h1>Hello Members!!</h1>
+      <Link href="/">Back Home</Link>
+    </div>
+  );
+}
